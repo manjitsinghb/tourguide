670496ca475fe5d4f7f4aa6a397befd7aa4d792a
manjit <Manjitsingh.bits@gmail.com>
2017-06-18 19:56:16 -0700

    Working route

--- tourguidedashboard/src/main/resources/static/app/mapsearch/googlemapsearch.component.js
+++ tourguidedashboard/src/main/resources/static/app/mapsearch/googlemapsearch.component.js
@@ -23,6 +23,7 @@ export class GoogleMapSearchAddress {
     }
 
     findMatchingAddress():Observable<MatchedAddress[]> {
+       this.showSuggestions=true;
         if(!this.address) {
             return;
         }
